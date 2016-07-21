model bug fix and test in progress. there's still a bug whlie gradient updating

main.py
@@ -16,9 +16,9 @@ flags = tf.app.flags
 # network
 flags.DEFINE_string("model", "pixel_rnn", "name of model [pixel_rnn, pixel_cnn]")
 flags.DEFINE_integer("batch_size", 100, "size of a batch")
-flags.DEFINE_integer("hidden_dims", 16, "dimesion of hidden states of LSTM or Conv layers")
+flags.DEFINE_integer("hidden_dims", 64, "dimesion of hidden states of LSTM or Conv layers")
 flags.DEFINE_integer("recurrent_length", 2, "the length of LSTM or Conv layers")
-flags.DEFINE_integer("out_hidden_dims", 32, "dimesion of hidden states of output Conv layers")
+flags.DEFINE_integer("out_hidden_dims", 64, "dimesion of hidden states of output Conv layers")
 flags.DEFINE_integer("out_recurrent_length", 2, "the length of output Conv layers")
 flags.DEFINE_boolean("use_residual", False, "whether to use residual connections or not")
 flags.DEFINE_boolean("use_dynamic_rnn", False, "whether to use dynamic_rnn or not")
@@ -28,8 +28,7 @@ flags.DEFINE_float("max_step", 100000, "# of step in an epoch")
 flags.DEFINE_float("test_step", 100, "# of step to test a model")
 flags.DEFINE_float("save_step", 1000, "# of step to save a model")
 flags.DEFINE_float("learning_rate", 1e-3, "learning rate")
-flags.DEFINE_float("max_grad", 1, "max of gradient")
-flags.DEFINE_float("min_grad", -1, "min of gradient")
+flags.DEFINE_float("grad_clip", 1, "value of gradient to be used for clipping")
 flags.DEFINE_string("data", "mnist", "name of dataset")
 
 # Debug
@@ -62,7 +61,7 @@ def main(_):
     height, width, channel = 28, 28, 1
 
   with tf.Session() as sess:
-    logger.info("Building %s." % model)
+    logger.info("Building %s starts!" % model)
 
     if data_format == "NHWC":
       input_shape = [None, height, width, channel]
@@ -73,67 +72,124 @@ def main(_):
 
     l = {}
 
-    l['inputs'] = tf.placeholder(tf.int32, [None, height, width, channel],)
-    l['normalized_inputs'] = tf.div(tf.to_float(l['inputs']), 255., name="normalized_inputs")
+    l['inputs'] = tf.placeholder(tf.float32, [None, height, width, channel],)
 
-    # residual connections of PixelRNN
-    scope = "conv_inputs"
-    logger.info("Building %s: %s" % (model, scope))
-
-    # main reccurent layers
-    if conf.use_residual and model == "pixel_rnn":
-      l['conv_inputs'] = conv2d(l['normalized_inputs'], conf.hidden_dims * 2, [7, 7], "A", scope=scope)
+    if conf.data =='mnist':
+      l['normalized_inputs'] = l['inputs']
     else:
-      l['conv_inputs'] = conv2d(l['normalized_inputs'], conf.hidden_dims, [7, 7], "A", scope=scope)
+      l['normalized_inputs'] = tf.div(l['inputs'], 255., name="normalized_inputs")
+
+    # input of main reccurent layers
+    scope = "conv_inputs"
+    logger.info("Building %s" % scope)
+
+    if conf.use_residual and model == "pixel_rnn":
+      l[scope] = conv2d(l['normalized_inputs'], conf.hidden_dims * 2, [7, 7], "A", scope=scope)
+    else:
+      l[scope] = conv2d(l['normalized_inputs'], conf.hidden_dims, [7, 7], "A", scope=scope)
     
+    # main reccurent layers
     l_hid = l[scope]
     for idx in xrange(conf.recurrent_length):
       if model == "pixel_rnn":
-        scope = 'diag_bilstm_%d' % idx
+        scope = 'LSTM%d' % idx
         l[scope] = l_hid = diagonal_bilstm(l_hid, conf, scope=scope)
       elif model == "pixel_cnn":
-        scope = 'conv2d_B_%d' % idx
+        scope = 'CONV%d' % idx
         l[scope] = l_hid = conv2d(l_hid, 3, [1, 1], "B", scope=scope)
-      logger.info("Building %s: %s" % (model, scope))
+      logger.info("Building %s" % scope)
 
     # output reccurent layers
     for idx in xrange(conf.out_recurrent_length):
-      scope = 'conv2d_out_%d' % idx
+      scope = 'CONV_OUT%d' % idx
       l[scope] = l_hid = tf.nn.relu(conv2d(l_hid, conf.out_hidden_dims, [1, 1], None, scope=scope))
-      logger.info("Building %s: %s" % (model, scope))
+      logger.info("Building %s" % scope)
 
-    scope = 'output'
     if channel == 1:
       l['conv2d_out_final'] = conv2d(l_hid, 1, [1, 1], None, scope='conv2d_out_final')
-      l[scope] = tf.nn.sigmoid(l['conv2d_out_final'])
+      l['output'] = tf.nn.sigmoid(l['conv2d_out_final'])
 
+      logger.info("Building loss and optims")
       loss = tf.reduce_mean(
           tf.nn.sigmoid_cross_entropy_with_logits(l['conv2d_out_final'], l['normalized_inputs'], name='loss'))
 
       optimizer = tf.train.RMSPropOptimizer(conf.learning_rate)
+      #optimizer = tf.train.AdamOptimizer(conf.learning_rate)
       grads_and_vars = optimizer.compute_gradients(loss)
 
       new_grads_and_vars = \
-          [(tf.clip_by_value(gv[0], conf.min_grad, conf.max_grad), gv[1]) for gv in grads_and_vars]
+          [(tf.clip_by_value(gv[0], -conf.grad_clip, conf.grad_clip), gv[1]) for gv in grads_and_vars]
       optim = optimizer.apply_gradients(new_grads_and_vars)
+      #optim = optimizer.minimize(loss)
     else:
       raise ValueError("Not implemented yet for RGB colors")
 
     logger.info("Building %s finished!" % model)
+    show_all_variables()
 
-    stat = Statistic(sess, conf.data, model_dir, tf.trainable_variables(), conf.test_step)
+    #stat = Statistic(sess, conf.data, model_dir, tf.trainable_variables(), conf.test_step)
+    stat = None
+    initial_step = stat.get_t() if stat else 0
 
+    logger.info("Initializing all variables")
     tf.initialize_all_variables().run()
 
-    iterator = trange(conf.max_step, ncols=70, initial=stat.get_t())
+    logger.info("Training starts!")
+
+    def binarize(images):
+      return (np.random.uniform(size=images.shape) < images).astype('float32')
+
+    G = tf.get_default_graph()
+
+    iterator = trange(conf.max_step, ncols=70, initial=initial_step)
     for i in iterator:
-      images = next_batch(conf.batch_size).reshape([conf.batch_size, height, width, channel])
-
-      _, cost = sess.run([optim, loss], feed_dict={l['inputs']: images})
-      iterator.set_description("l: %s" % cost)
+      #images = binarize(next_batch(conf.batch_size)).reshape([conf.batch_size, height, width, channel])
+      images = np.ones([100, 28, 28, 1])
+      images[:,10:20,:,:]=0
+      _, cost, output, output_final = sess.run([optim, loss, l['output'], l['conv2d_out_final']], feed_dict={l['inputs']: images})
       print
+      print mprint(images[1])
+      print mprint(output[1], 0.5)
+      print mprint(output_final[1], 0.5)
 
-      stat.on_step(cost)
+      #cost, out1, out2, conv = sess.run(
+      #    [loss, l['output'], l['conv2d_out_final'], l['conv_inputs']], feed_dict={l['inputs']: images})
+      #print
+      #print out1[0][10][:9].tolist()
+      #print out2[0][10][:9].tolist()
+      #print images[0][10][:9].tolist()
+      #print conv[0][10][:9,3].tolist()
+      #print out1[0].max(), out1[0].min(), out2[0].max(), out2[0].min()
+
+      #_, cost, out1, out2, conv = sess.run(
+      #    [optim, loss, l['output'], l['conv2d_out_final'], l['conv_inputs']], feed_dict={l['inputs']: images})
+      #print
+      #print out1[0][10][:9].tolist()
+      #print out2[0][10][:9].tolist()
+      #print images[0][10][:9].tolist()
+      #print conv[0][10][:9,3].tolist()
+      #print out1[0].max(), out1[0].min(), out2[0].max(), out2[0].min()
+      #import ipdb; ipdb.set_trace() 
+
+      # G.get_collection('conv2d_weights')[0].eval()[:,:,0,0]
+
+      # tf.get_collection('lstm_inputs')[0].eval({l['inputs']: images})[0,:,:,0]
+      # mprint(tf.get_collection('lstm_inputs')[0].eval({l['inputs']: images})[1,:,:,0], 0.0007746)
+      # mprint(tf.get_collection('skewed_lstm_inputs')[0].eval({l['inputs']: images})[1,:,:,0], 0.0007746)
+      # tf.get_collection('skewed_conv_inputs')[0].eval({l['inputs']: images})[1,:,:,0]
+      # mprint(tf.get_collection('skewed_conv_inputs')[0].eval({l['inputs']: images})[1,:,:,0], 0)
+
+      # mprint(G.get_collection('column_wise_inputs')[0].eval({l['inputs']: images})[1,:,:,0], 0)
+      # mprint(G.get_collection('rnn_inputs')[0].eval({l['inputs']: images})[1,:,::4*64],0)
+      # for i in xrange(55): mprint([tf.get_collection('i_to_s')[i].eval({l['inputs']: images})[1,::4*64]], 0)
+
+      # tf.get_collection('conv2d_outputs')[0].eval({l['inputs']: images})
+      # mprint(tf.get_collection('conv2d_outputs')[0].eval({l['inputs']: images})[1,:,:,1], 0)
+      # mprint(tf.get_collection('output_state_fw')[0].eval({l['inputs']: images})[1,:,:,-1], 0.3)
+
+      if stat:
+        stat.on_step(cost)
+      iterator.set_description("l: %s" % cost)
 
 if __name__ == "__main__":
   tf.app.run()

ops.py
@@ -7,6 +7,9 @@ from tensorflow.python.ops import rnn_cell
 from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.contrib.layers import variance_scaling_initializer
 
+#WEIGHT_INITIALIZER = tf.contrib.layers.xavier_initializer
+WEIGHT_INITIALIZER = tf.uniform_unit_scaling_initializer
+
 logger = logging.getLogger(__name__)
 
 he_uniform = variance_scaling_initializer(factor=2.0, mode="FAN_IN", uniform=False)
@@ -74,7 +77,7 @@ def conv2d(
     strides=[1, 1], # [column_wise_stride, row_wise_stride]
     padding="SAME",
     activation_fn=tf.nn.relu,
-    weights_initializer=tf.contrib.layers.xavier_initializer(),
+    weights_initializer=WEIGHT_INITIALIZER(),
     weights_regularizer=None,
     biases_initializer=tf.zeros_initializer,
     biases_regularizer=None,
@@ -106,9 +109,11 @@ def conv2d(
         mask[center_h,center_w,:,:] = 0.
 
       weights *= tf.constant(mask, dtype=tf.float32)
+      tf.add_to_collection('conv2d_weights', weights)
 
     outputs = tf.nn.conv2d(inputs,
         weights, [1, stride_h, stride_w, 1], padding=padding, name='outputs')
+    tf.add_to_collection('conv2d_outputs', outputs)
 
     if biases_initializer != None:
       biases = tf.get_variable("biases", [num_outputs,],
@@ -130,7 +135,7 @@ def conv1d(
     strides=[1, 1], # [column_wise_stride, row_wise_stride]
     padding="SAME",
     activation_fn=tf.nn.relu,
-    weights_initializer=tf.contrib.layers.xavier_initializer(),
+    weights_initializer=WEIGHT_INITIALIZER(),
     weights_regularizer=None,
     biases_initializer=tf.zeros_initializer,
     biases_regularizer=None,
@@ -144,9 +149,11 @@ def conv1d(
     weights_shape = [kernel_h, kernel_w, channel, num_outputs]
     weights = tf.get_variable("weights", weights_shape,
       tf.float32, weights_initializer, weights_regularizer)
+    tf.add_to_collection('conv1d_weights', weights)
 
     outputs = tf.nn.conv2d(inputs, 
         weights, [1, stride_h, stride_w, 1], padding=padding, name='outputs')
+    tf.add_to_collection('conv1d_outputs', weights)
 
     if biases_initializer != None:
       biases = tf.get_variable("biases", [num_outputs,],
@@ -169,12 +176,20 @@ def diagonal_bilstm(inputs, conf, scope='diagonal_bilstm'):
     output_state_fw = diagonal_lstm(inputs, conf, scope='output_state_fw')
     output_state_bw = reverse(diagonal_lstm(reverse(inputs), conf, scope='output_state_bw'))
 
+    tf.add_to_collection('output_state_fw', output_state_fw)
+    tf.add_to_collection('output_state_bw', output_state_bw)
+
     if conf.use_residual:
       residual_state_fw = conv2d(output_state_fw, conf.hidden_dims * 2, [1, 1], "B", scope="residual_fw")
       output_state_fw = residual_state_fw + inputs
 
       residual_state_bw = conv2d(output_state_bw, conf.hidden_dims * 2, [1, 1], "B", scope="residual_bw")
       output_state_bw = residual_state_bw + inputs
+
+      tf.add_to_collection('residual_state_fw', residual_state_fw)
+      tf.add_to_collection('residual_state_bw', residual_state_bw)
+      tf.add_to_collection('residual_output_state_fw', output_state_fw)
+      tf.add_to_collection('residual_output_state_bw', output_state_bw)
 
     batch, height, width, channel = get_shape(output_state_bw)
 
@@ -184,16 +199,24 @@ def diagonal_bilstm(inputs, conf, scope='diagonal_bilstm'):
 
     output_state_bw_with_last_zeros = tf.concat(1, [output_state_bw_except_last, dummy_zeros])
 
+    tf.add_to_collection('output_state_bw_with_last_zeros', output_state_bw_with_last_zeros)
+
     return output_state_fw + output_state_bw_with_last_zeros
 
 def diagonal_lstm(inputs, conf, scope='diagonal_lstm'):
   with tf.variable_scope(scope):
+    tf.add_to_collection('lstm_inputs', inputs)
+
     skewed_inputs = skew(inputs, scope="skewed_i")
+    tf.add_to_collection('skewed_lstm_inputs', skewed_inputs)
 
     # input-to-state (K_is * x_i) : 1x1 convolution. generate 4h x n x n tensor.
     input_to_state = conv2d(skewed_inputs, conf.hidden_dims * 4, [1, 1], "B", scope="i_to_s")
     column_wise_inputs = tf.transpose(
-        input_to_state, [2, 0, 1, 3]) # [width, batch, height, hidden_dims * 4]
+        input_to_state, [0, 2, 1, 3]) # [batch, width, height, hidden_dims * 4]
+
+    tf.add_to_collection('skewed_conv_inputs', input_to_state)
+    tf.add_to_collection('column_wise_inputs', column_wise_inputs)
 
     if conf.log_level == 'DEBUG':
       logger.warning("[assert] check equal of skew and unskew")
@@ -202,9 +225,11 @@ def diagonal_lstm(inputs, conf, scope='diagonal_lstm'):
       skew_assert_op = tf.Assert(tf.equal(inputs, unskewed_inputs, 'skew_check'), [unskewed_inputs])
       input_to_state = tf.with_dependencies([skew_assert_op], input_to_state)
 
-    width, batch, height, channel = get_shape(column_wise_inputs)
+    batch, width, height, channel = get_shape(column_wise_inputs)
     rnn_inputs = tf.reshape(column_wise_inputs,
         [-1, width, height * channel]) # [batch, max_time, height * hidden_dims * 4]
+
+    tf.add_to_collection('rnn_inputs', rnn_inputs)
 
     rnn_input_list = [tf.squeeze(rnn_input, squeeze_dims=[1]) 
         for rnn_input in tf.split(split_dim=1, num_split=width, value=rnn_inputs)]
@@ -223,7 +248,10 @@ def diagonal_lstm(inputs, conf, scope='diagonal_lstm'):
           [-1, width, height, conf.hidden_dims]) # [batch, width, height, hidden_dims]
 
       skewed_outputs = tf.transpose(width_first_outputs, [0, 2, 1, 3])
+      tf.add_to_collection('skewed_outputs', skewed_outputs)
+
       outputs = unskew(skewed_outputs)
+      tf.add_to_collection('unskewed_outputs', outputs)
 
     return outputs
 
@@ -263,10 +291,16 @@ class DiagonalLSTMCell(rnn_cell.RNNCell):
       conv1d_inputs = tf.reshape(h_prev,
           [-1, self._height, 1, self._hidden_dims], name='conv1d_inputs') # [batch, height, 1, hidden_dims]
 
+      tf.add_to_collection('i_to_s', i_to_s)
+      tf.add_to_collection('conv1d_inputs', conv1d_inputs)
+
       conv_s_to_s = conv1d(conv1d_inputs,
           4 * self._hidden_dims, 2, scope='s_to_s') # [batch, height, 1, hidden_dims * 4]
       s_to_s = tf.reshape(conv_s_to_s,
           [-1, self._height * self._hidden_dims * 4]) # [batch, height * hidden_dims * 4]
+
+      tf.add_to_collection('conv_s_to_s', conv_s_to_s)
+      tf.add_to_collection('s_to_s', s_to_s)
 
       lstm_matrix = tf.sigmoid(s_to_s + i_to_s)
 